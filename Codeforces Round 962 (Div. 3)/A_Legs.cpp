93b9e6501f983e9ee8c8bc031071c6932752b59b
tajuar-akash-hub <tajuar.akash.com>
2024-07-28 11:08:06 +0600

    added div3 round 962

---
+++ Codeforces Round 962 (Div. 3)/A_Legs.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int test; 
+    cin>>test;
+    int cows,chickesn;
+    cows=4,chickesn=2;
+
+    while(test--){
+        int legs;
+        cin>> legs;
+
+        if(legs%4==0){
+            cout<<legs/4<<endl;
+        }
+
+        else{
+            cout<<((legs/4)+1)<<endl;
+
+        }
+
+        
+
+
+
+        
+
+        
+
+        
+
+
+    }
+    
+}
